b21c7011e59f5a3d757c3ade7d78f89ae40a98c6
Qbicz <kubicz@student.agh.edu.pl>
2016-11-23 19:20:10 +0100

    calculating moments on video from camera

--- PC/camera/capture.cpp
+++ PC/camera/capture.cpp
@@ -1,5 +1,9 @@
 #include "opencv2/opencv.hpp"
+
 using namespace cv;
+int thresh = 60;
+RNG rng(12345);
+
 int main(int, char**)
 {
     VideoCapture cap(0); // open the default camera
@@ -13,10 +17,51 @@ int main(int, char**)
         cap >> frame; // get a new frame from camera
         cvtColor(frame, edges, COLOR_BGR2GRAY);
         GaussianBlur(edges, edges, Size(7,7), 1.5, 1.5);
-        Canny(edges, edges, 0, 30, 3);
+        //Canny(edges, edges, 0, 30, 3);
+
+        //getMoments();
+        Mat canny_output;
+        vector<vector<Point> > contours;
+        vector<Vec4i> hierarchy;
+
+        /// Detect edges using canny
+        Canny( edges, canny_output, thresh, thresh*2, 3 );
+        /// Find contours
+        findContours( canny_output, contours, hierarchy, CV_RETR_TREE, CV_CHAIN_APPROX_SIMPLE, Point(0, 0) );
+
+        /// Get the moments
+        vector<Moments> mu(contours.size() );
+        for( int i = 0; i < contours.size(); i++ )
+           { mu[i] = moments( contours[i], false ); }
+
+        ///  Get the mass centers:
+        vector<Point2f> mc( contours.size() );
+        for( int i = 0; i < contours.size(); i++ )
+           { mc[i] = Point2f( mu[i].m10/mu[i].m00 , mu[i].m01/mu[i].m00 ); }
+
+        /// Draw contours
+        Mat drawing = Mat::zeros( canny_output.size(), CV_8UC3 );
+        for( int i = 0; i< contours.size(); i++ )
+           {
+             Scalar color = Scalar( rng.uniform(0, 255), rng.uniform(0,255), rng.uniform(0,255) );
+             drawContours( drawing, contours, i, color, 2, 8, hierarchy, 0, Point() );
+             circle( drawing, mc[i], 4, color, -1, 8, 0 );
+           }
+
+        /// Show in a window
+        namedWindow( "Contours", CV_WINDOW_AUTOSIZE );
+        imshow( "Contours", drawing );
+
         imshow("edges", edges);
         if(waitKey(30) >= 0) break;
     }
     // the camera will be deinitialized automatically in VideoCapture destructor
     return 0;
+}
+
+// in callback only change threshold. perform all operations in loop
+
+void getMoments()
+{
+    // all moments code
 }
