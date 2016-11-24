b3990bfc9c006407ed59d871917061806e435bd3
Qbicz <kubicz@student.agh.edu.pl>
2016-11-24 10:06:02 +0100

    added Hu moments to camera code

--- PC/camera/capture.cpp
+++ PC/camera/capture.cpp
@@ -1,7 +1,7 @@
 #include "opencv2/opencv.hpp"
 
 using namespace cv;
-int thresh = 60;
+int thresh = 160;
 RNG rng(12345);
 
 int main(int, char**)
@@ -9,14 +9,14 @@ int main(int, char**)
     VideoCapture cap(0); // open the default camera
     if(!cap.isOpened())  // check if we succeeded
         return -1;
-    Mat edges;
+    Mat gray;
     namedWindow("edges",1);
     for(;;)
     {
         Mat frame;
         cap >> frame; // get a new frame from camera
-        cvtColor(frame, edges, COLOR_BGR2GRAY);
-        GaussianBlur(edges, edges, Size(7,7), 1.5, 1.5);
+        cvtColor(frame, gray, COLOR_BGR2GRAY);
+        GaussianBlur(gray, gray, Size(7,7), 1.5, 1.5);
         //Canny(edges, edges, 0, 30, 3);
 
         //getMoments();
@@ -25,7 +25,7 @@ int main(int, char**)
         vector<Vec4i> hierarchy;
 
         /// Detect edges using canny
-        Canny( edges, canny_output, thresh, thresh*2, 3 );
+        Canny( gray, canny_output, thresh, thresh*2, 3 );
         /// Find contours
         findContours( canny_output, contours, hierarchy, CV_RETR_TREE, CV_CHAIN_APPROX_SIMPLE, Point(0, 0) );
 
@@ -33,6 +33,20 @@ int main(int, char**)
         vector<Moments> mu(contours.size() );
         for( int i = 0; i < contours.size(); i++ )
            { mu[i] = moments( contours[i], false ); }
+
+        /// Compute Hu moments - use to tell the difference between mirrored objects/rotated 180 degrees
+        for( int i = 0; i < contours.size(); i++)
+        {
+            Moments mom = mu[i];
+            double hu[7];
+            HuMoments(mom, hu);
+            printf("Hu invariants for contour %d:\n", i);
+            for( int i = 0; i < 7; i++ )
+               printf("[%d]=%.4e ", i+1, hu[i]);
+            printf("\n");
+        }
+
+        // TODO: draw 7th Hu moment as an arrow on the frame
 
         ///  Get the mass centers:
         vector<Point2f> mc( contours.size() );
@@ -52,7 +66,7 @@ int main(int, char**)
         namedWindow( "Contours", CV_WINDOW_AUTOSIZE );
         imshow( "Contours", drawing );
 
-        imshow("edges", edges);
+        imshow("edges", canny_output);
         if(waitKey(30) >= 0) break;
     }
     // the camera will be deinitialized automatically in VideoCapture destructor
