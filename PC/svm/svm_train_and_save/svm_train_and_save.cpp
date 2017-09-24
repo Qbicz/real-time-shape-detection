9e9ebf7331a13c60e62bab208e2b69bd20d7f3bf
Qbicz <avenger.v14@gmail.com>
2017-09-24 16:41:59 +0200

    add SVM training code with saving SVM to a file

---
+++ PC/svm/svm_train_and_save/svm_train_and_save.cpp
@@ -0,0 +1,28 @@
+#include <opencv2/core/core.hpp>
+#include <opencv2/highgui/highgui.hpp>
+#include <opencv2/ml/ml.hpp>
+
+using namespace cv;
+
+int main()
+{
+    // Set up training data
+    float labels[4] = {1.0, 1.0, -1.0, 1.0};
+    Mat labelsMat(4, 1, CV_32FC1, labels);
+
+    float trainingData[4][2] = { {501, 10}, {255, 10}, {501, 255}, {10, 501} };
+    Mat trainingDataMat(4, 2, CV_32FC1, trainingData);
+
+    // Set up SVM's parameters
+    CvSVMParams params;
+    params.svm_type    = CvSVM::C_SVC;
+    params.kernel_type = CvSVM::LINEAR;
+    params.term_crit   = cvTermCriteria(CV_TERMCRIT_ITER, 100, 1e-6);
+
+    // Train the SVM
+    CvSVM SVM;
+    SVM.train(trainingDataMat, labelsMat, Mat(), Mat(), params);
+
+    SVM.save("trained.svm");
+}
+
