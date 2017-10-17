a9042ec453ef8ab795940d8c7e77aece4665ecda
Piotr Palucki <piotrek.palucki@gmail.com>
2017-10-17 22:34:49 +0200

    Added simple prediction for testing

--- PC/svm/svm_test/svm_test.cpp
+++ PC/svm/svm_test/svm_test.cpp
@@ -1,26 +1,47 @@
-#include <fstream>
-#include <vector>
-
 #include <opencv2/core/core.hpp>
 #include <opencv2/highgui/highgui.hpp>
 #include <opencv2/ml/ml.hpp>
+#include <fstream>
 
-#include <json.hpp>
 #include "common.h"
+
+//see https://docs.opencv.org/2.4/modules/ml/doc/support_vector_machines.html#cvsvm-predict for reference
 
 #define DATA_DIMENSIONS 2 // for Hu moments, it will be 7
 
 using namespace cv;
 using json = nlohmann::json;
 
-int main()
+int main(int argc, char* argv[])
 {
-    // Read input training data to JSON object
-    std::ifstream input_training_data("../data/dataset_training.json");
+    if(argc < 2)
+    {
+        std::cout << "Please provide trained svm file\n";
+        exit(1);
+    }
+
+    std::string svmFile(argv[1]);
+    std::cout << "Loading SVM from file " << svmFile  << "\n";
+
+    CvSVM svm;
+    
+    svm.load(svmFile.c_str());
+    auto c = svm.get_support_vector_count();
+
+    std::cout << "Successfully loaded with parameters:\n";
+    
+    for (int i = 0; i < c; ++i)
+    {
+        const float* v = svm.get_support_vector(i);
+        std::cout << "Support vector #" << i << " " << *v << "\n";
+    }
+
+// Read input training data to JSON object
+    std::ifstream test_data("../data/dataset_training.json");
     json training_data_json;
-    input_training_data >> training_data_json;
+    test_data >> training_data_json;
  
-    std::cout << "Training data set: " << training_data_json << std::endl;
+    std::cout << "Test data set: " << training_data_json << std::endl;
     std::cout << "Data set size: " << training_data_json.size() << std::endl;
 
     // Set up training data
@@ -52,16 +73,10 @@ int main()
     assert(labels.size() == training_data_json.size());
     Mat labels_mat(labels);
 
-    // Set up SVM's parameters
-    CvSVMParams params;
-    params.svm_type    = CvSVM::C_SVC;
-    params.kernel_type = CvSVM::LINEAR;
-    params.term_crit   = cvTermCriteria(CV_TERMCRIT_ITER, 100, 1e-6);
+    svm.predict(training_data_mat, labels_mat);
 
-    // Train the SVM
-    CvSVM SVM;
-    SVM.train(training_data_mat, labels_mat, Mat(), Mat(), params);
 
-    SVM.save("trained.svm");
+
+
+    return 0;
 }
-
