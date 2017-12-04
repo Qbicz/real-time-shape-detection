37338473d5d4a72921900c96cd4b73bfea84353d
Qbicz <avenger.v14@gmail.com>
2017-12-04 09:54:47 +0100

    SVM: feed labeled acorn Hu moments and train SVM

--- PC/svm/svm_train_and_save/svm_train_and_save.cpp
+++ PC/svm/svm_train_and_save/svm_train_and_save.cpp
@@ -13,31 +13,27 @@
 using namespace cv;
 using json = nlohmann::json;
 
-static std::vector<float> svm_build_single_element(json &element)
-{
-    std::vector<float> training_element;
-    std::cout << "element: " << element << std::endl; 
-    training_element.push_back(element["hu"]);
-    return training_element;
-}
-
 int main()
 {
-    // Read input training data to JSON object
+    // Prepare file with input training data
     std::ifstream input_training_data("../data/dataset_training_labeled_canny150.json");
+    // Read training data to JSON object
     json training_data_json;
     input_training_data >> training_data_json;
- 
-    std::cout << "Training data set: " << training_data_json << std::endl;
+
+    std::cout << "Training data set: " << training_data_json.dump(4) << std::endl;
     std::cout << "Data set size: " << training_data_json.size() << std::endl;
 
     // Set up training data
     std::vector<float> labels;
     std::vector<float> training_data;
+    unsigned int labeled_dataset_size = 0;
 
+    // For each acorn read its learning label and Hu moments
     for (auto element : training_data_json)
-    {
+{
         // Ignore not labeled elements
+        std::cout << "element[\"label\"]" << element["label"] << std::endl;
         if (element["label"] == 0)
         {
             continue;
@@ -46,26 +42,36 @@ int main()
         {
             // Read training label
             labels.push_back(element["label"]);
+            labeled_dataset_size++;
         }
 
         // Read training data
-        std::vector<float> training_element = svm_build_single_element(element);
-        // Put new element at the end of vector containing all data
-        training_data.insert(training_data.end(),
-                             training_element.begin(),
-                             training_element.end()
-                            );
+        std::vector<float> training_element = string_to_vector(element["hu_moments"]);
+
+        std::cout << "training_data" << std::endl;
         print_vector(training_data);
-    }
+        // Put new element at the end of vector containing all data. The data will be then reshaped.
+        if (training_element.size() > 0)
+        {
+            training_data.insert(
+                                 training_data.end(),
+                                 training_element.begin(),
+                                 training_element.end()
+                                );
+        }
+        std::cout << "\nTraining element ";
+        print_vector(training_data);
+        std::cout << "\n\n";
+}
 
     // Create a matrix from vector and reshape matrix to have one training element in one row
     // It is done this way because there's no conversion between vector<vector<float> > and cv::Mat
-    assert(DATA_DIMENSIONS*training_data_json.size() == training_data.size());
-    Mat training_data_mat = Mat(training_data).reshape(0, training_data_json.size());
+    assert(DATA_DIMENSIONS*labeled_dataset_size == training_data.size());
+    Mat training_data_mat = Mat(training_data).reshape(0, labeled_dataset_size);
 
     // Put training data labels in format for OpenCV SVM
     print_vector(labels);
-    assert(labels.size() == training_data_json.size());
+    assert(labels.size() == labeled_dataset_size);
     Mat labels_mat(labels);
 
     // Set up SVM's parameters
