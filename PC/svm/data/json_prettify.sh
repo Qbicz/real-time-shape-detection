7c8016d461b0233cfefc172b02d683ee49d2f188
Qbicz <avenger.v14@gmail.com>
2018-01-06 17:43:53 +0100

    json: prevent script from destroying original JSON data

--- PC/svm/data/json_prettify.sh
+++ PC/svm/data/json_prettify.sh
@@ -1,3 +1,4 @@
 DATASET_NAME=$1
 echo $DATASET_NAME
-cat $DATASET_NAME | python -m json.tool > $DATASET_NAME
+echo "Prettify..."
+cat $DATASET_NAME | python -m json.tool > "prettified_$DATASET_NAME"
